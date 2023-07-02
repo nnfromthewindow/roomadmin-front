cb10a77e6d7d384337ae0f6d5b9a419fde91b0d4
Nicolas Nuccelli <nuccelli@hotmail.com>
2023-07-02 01:16:05 -0300

    login page optimization, lazy loading image

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { registerLicense } from '@syncfusion/ej2-base';
-
-// Registering Syncfusion license key
-registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
