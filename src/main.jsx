128a928b28fca99a7e15e77775058bda76e5cfc0
Nicolas Nuccelli <nuccelli@hotmail.com>
2023-07-07 23:42:58 -0300

    deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
