149463163a7721bf3c6effea47b0007196def246
Nicolas Nuccelli <nuccelli@hotmail.com>
2023-07-08 16:00:26 -0300

    base url updated

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { setCredentials,logOut } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://roomyapp.onrender.com',
+    baseUrl: 'https://roomyapp-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers,{getState}) => {
         const token = getState().auth.token
